9a26062a6623573604a6d113dda6f2f7471aced3
Fadilix <iamfadile2@gmail.com>
2024-05-21 18:16:30 +0000

    first commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTodoApp.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public void OnModelCreating()
+        {
+
+        }
+        //public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorTodoApp.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("title")]
+        [StringLength(100, ErrorMessage = "Should not be more than 100")]
+        public string Title { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("is_completed")]
+        public bool IsCompleted { get; set; }
+
+        
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
